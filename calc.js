372391b6b7c378f6fcf826208fc2c4213bbc9d78
elifnurozkaya <elifnurozkaya4334@gmail.com>
2025-08-10 19:11:54 +0300

    select the buttons

--- calc.js
+++ calc.js
@@ -1,6 +1,27 @@
 const buttons = document.querySelectorAll("button");
 const operators= Array.from(document.querySelectorAll(".operator"));
 const deleteButtons = Array.from(document.querySelectorAll(".delete"));
+const topScreen = document.querySelector(".typeScreen");
+const bottomScreen = document.querySelector(".resultScreen");
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const multi = document.querySelector("#mult");
+const divi = document.querySelector("#div");
+const equal = document.querySelector("#equalTo");
+const CE = document.querySelector("#clearEntry");
+const AC = document.querySelector("#allClear");
+const bs = document.querySelector("#backspace");
+const dot = document.querySelector("#dot");
 
 buttons.forEach(button => {
 button.addEventListener("mousedown",(e)=> {
@@ -20,6 +41,14 @@ buttons.forEach(button => {
 
 
 
+
+
+
+
+
+
+
+
 function releaseButton(e){
     const button = e.currentTarget;
         button.style.boxShadow = "inset 0rem -0.3rem 0.2rem black";
@@ -32,7 +61,20 @@ function releaseButton(e){
         button.style.backgroundColor= "rgb(255, 247, 212)";
 }
     
-
+function add(a,b){
+    return a+b;
+}
+function substract(a,b){
+    return a-b;
+}
+function multiplt(a,b){
+    return a*b;
+}
+function divide(a,b){
+    if(b == 0)
+        return("Yk you can't do that!")
+    return a/b;
+}
 
 
 
